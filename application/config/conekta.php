a87906401612e809b8a937660bd2f43a4b0043ec
luis <luis@user1>
2015-07-08 00:39:24 -0500

    -- implementacion de conekta con CI

--- application/config/conekta.php
+++ application/config/conekta.php
@@ -7,8 +7,8 @@
 | These details are used to authenticate your Conekta API.
 |--------------------------------------------------------------------------
 */
-$config['conekta_api_key_private'] = 'key_JCzF4o6NbpVWiCUgTEStyQ';
-$config['conekta_api_key_public'] = 'key_MNcGCEvSLqzgQ2StUks3JaQ';
+$config['conekta_api_key_private'] = 'key_private';
+$config['conekta_api_key_public'] = 'key_public';
 $config['conekta_api_version'] = "1.0.0";
 
 /* End of file conekta.php */
